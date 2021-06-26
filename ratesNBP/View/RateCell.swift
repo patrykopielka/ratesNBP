3c7a61e7a569b34d9d69f91f3abca3bdddf2b493
Patryk Opiełka <patrykopielka@MacBook-Air-Patryk.local>
2021-06-26 13:57:55 +0200

    update UI

--- ratesNBP/View/RateCell.swift
+++ ratesNBP/View/RateCell.swift
@@ -9,24 +9,18 @@ import UIKit
 
 class RateCell: UITableViewCell {
     
-    
-    
     @IBOutlet weak var rateCode: UILabel!
     @IBOutlet weak var rateName: UILabel!
     @IBOutlet weak var rateMid: UILabel!
     @IBOutlet weak var rateAsk: UILabel!
     @IBOutlet weak var rateDate: UILabel!
     
-
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
 }
